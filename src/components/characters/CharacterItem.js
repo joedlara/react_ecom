abea3d92eb06dbf654ac0f4376abb4635a3dfdaf
Joed Lara <joedlara@Joeds-MacBook-Pro.local>
2020-07-11 11:24:21 -0700

    first commit

---
+++ src/components/characters/CharacterItem.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CharacterItem = ({item}) => {
+    return (
+    <div>{item.name}</div>
+    )
+}
+
+export default CharacterItem
---
+++ src/components/ui/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header className='center'>
+            <h1>We out here</h1>
+        </header>
+    )
+}
+
+export default Header
